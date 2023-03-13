ea0611f874d27f400e0f83c8b39b5f8eb4e756c5
Farzan Nedaei <Nedaei.F@Orchidpharmed.com>
2023-03-13 14:15:16 +0330

    Work on video-30

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'productsApp/ProductsIndex'
-import 'cartApp/CartShow'
+import { mount as productsMount } from 'productsApp/ProductsIndex'
+import { mount as cartMount } from 'cartApp/CartShow'
 
 console.log('Container!')
+
+productsMount(document.querySelector('#container-products'))
+cartMount(document.querySelector('#container-cart'))
